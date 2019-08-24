e5444a67db0cf81d490370376590bc1f61d9d05f
jamaljsr <jamal@farrock.com>
2019-08-24 19:16:20 -0400

    feat(network-view): split NetworkView into separate components

---
+++ src/components/network/LndCard.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { LightningNode } from 'types';
+import { Card, Avatar, Icon } from 'antd';
+import { StatusBadge, DetailsList } from 'components/common';
+import lnd from 'resources/lnd.png';
+
+interface Props {
+  node: LightningNode;
+}
+
+const lndDetails = [
+  { label: 'PubKey', value: '0245....5fd47' },
+  { label: 'Host', value: '159.65.239.204:9735' },
+  { label: 'Channels', value: '2' },
+  { label: 'Synced to Chain', value: 'true' },
+  { label: 'Chain Node', value: 'bitcoind1' },
+  { label: 'Version', value: 'v0.7.1' },
+];
+
+const LndCard: React.FC<Props> = ({ node }) => {
+  return (
+    <Card
+      title={<StatusBadge status={node.status} text={node.name} />}
+      extra={<Avatar src={lnd} shape="square" size="small" />}
+      actions={[
+        <Icon type="code" key="code" />,
+        <Icon type="file-text" key="logs" />,
+        <Icon type="ellipsis" key="ellipsis" />,
+      ]}
+    >
+      <DetailsList details={lndDetails} />
+    </Card>
+  );
+};
+
+export default LndCard;
---
+++ src/components/network/NetworkActions.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Status } from 'types';
+import { Menu, Icon, Button, Dropdown } from 'antd';
+import { ButtonType } from 'antd/lib/button';
+
+interface Props {
+  status: Status;
+}
+
+const config: {
+  [key: number]: {
+    label: string;
+    type: ButtonType;
+    icon: string;
+  };
+} = {
+  [Status.Starting]: {
+    label: 'Starting',
+    type: 'primary',
+    icon: '',
+  },
+  [Status.Started]: {
+    label: 'Stop',
+    type: 'default',
+    icon: 'stop',
+  },
+  [Status.Stopping]: {
+    label: 'Stopping',
+    type: 'default',
+    icon: '',
+  },
+  [Status.Stopped]: {
+    label: 'Start',
+    type: 'primary',
+    icon: 'play-circle',
+  },
+  [Status.Error]: {
+    label: 'Stop',
+    type: 'danger',
+    icon: 'warning',
+  },
+};
+
+const NetworkActions: React.FC<Props> = ({ status }) => {
+  const loading = status === Status.Starting || status === Status.Stopping;
+  const { label, type, icon } = config[status];
+  const menu = (
+    <Menu theme="dark">
+      <Menu.Item key="1">
+        <Icon type="form" />
+        Edit
+      </Menu.Item>
+      <Menu.Item key="2">
+        <Icon type="close" />
+        Delete
+      </Menu.Item>
+    </Menu>
+  );
+
+  return (
+    <>
+      <Button key="start" type={type} icon={icon} loading={loading}>
+        {label}
+      </Button>
+      <Dropdown key="options" overlay={menu}>
+        <Button icon="down" />
+      </Dropdown>
+    </>
+  );
+};
+
+export default NetworkActions;
