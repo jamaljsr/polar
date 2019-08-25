620c9d1bcacabb161717f10f4a9f44e30be81f79
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2019-08-25 10:27:07 -0400

    test(network-view): fix unit tests broken by translations update

--- src/components/common/StatusBadge.spec.tsx
+++ src/components/common/StatusBadge.spec.tsx
@@ -10,26 +10,26 @@ describe('StatusTag Component', () => {
 
   it('should render the Starting status', () => {
     const { getByTestId } = renderComponent(Status.Starting);
-    expect(getByTestId('tag')).toHaveTextContent('Starting');
+    expect(getByTestId('tag')).toHaveTextContent('cmps.status-tag.status-starting');
   });
 
   it('should render the Started status', () => {
     const { getByTestId } = renderComponent(Status.Started);
-    expect(getByTestId('tag')).toHaveTextContent('Started');
+    expect(getByTestId('tag')).toHaveTextContent('cmps.status-tag.status-started');
   });
 
   it('should render the Stopping status', () => {
     const { getByTestId } = renderComponent(Status.Stopping);
-    expect(getByTestId('tag')).toHaveTextContent('Stopping');
+    expect(getByTestId('tag')).toHaveTextContent('cmps.status-tag.status-stopping');
   });
 
   it('should render the Stopped status', () => {
     const { getByTestId } = renderComponent(Status.Stopped);
-    expect(getByTestId('tag')).toHaveTextContent('Stopped');
+    expect(getByTestId('tag')).toHaveTextContent('cmps.status-tag.status-stopped');
   });
 
   it('should render the Error status', () => {
     const { getByTestId } = renderComponent(Status.Error);
-    expect(getByTestId('tag')).toHaveTextContent('Error');
+    expect(getByTestId('tag')).toHaveTextContent('cmps.status-tag.status-error');
   });
 });
