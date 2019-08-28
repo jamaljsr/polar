1b14fc8ac460a76db83da03b1d702f80d444f732
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2019-08-28 08:00:46 -0400

    test(cards): update unit tests for node cards

--- src/components/network/BitcoinCard.spec.tsx
+++ src/components/network/BitcoinCard.spec.tsx
@@ -4,14 +4,6 @@ import { BitcoinNode, Status } from 'types';
 import BitcoindCard from './BitcoindCard';
 
 describe('StatusTag Component', () => {
-  const node: BitcoinNode = {
-    id: 1,
-    name: 'test btc',
-    implementation: 'bitcoind',
-    status: Status.Stopped,
-    type: 'bitcoin',
-  };
-
   const details = [
     { label: 'Block Height', value: '432' },
     { label: 'Wallet Balance', value: '54.00000000' },
@@ -19,17 +11,30 @@ describe('StatusTag Component', () => {
     { label: 'Version', value: 'v0.18.1' },
   ];
 
-  const renderComponent = () => {
+  const renderComponent = (status: Status) => {
+    const node: BitcoinNode = {
+      id: 1,
+      name: 'test btc',
+      implementation: 'bitcoind',
+      status,
+      type: 'bitcoin',
+    };
+
     return renderWithProviders(<BitcoindCard node={node} details={details} />);
   };
 
   it('should render the name', () => {
-    const { getByText } = renderComponent();
-    expect(getByText(node.name)).toBeTruthy();
+    const { getByText } = renderComponent(Status.Stopped);
+    expect(getByText('test btc')).toBeTruthy();
   });
 
-  it('should render the details', () => {
-    const { getByText } = renderComponent();
+  it('should display Stopped when node is stopped', () => {
+    const { getByText } = renderComponent(Status.Stopped);
+    expect(getByText('Stopped')).toBeTruthy();
+  });
+
+  it('should render the details when node is started', () => {
+    const { getByText } = renderComponent(Status.Started);
     expect(getByText(details[0].label)).toBeTruthy();
     expect(getByText(details[0].value)).toBeTruthy();
     expect(getByText(details[1].label)).toBeTruthy();
--- src/components/network/LndCard.spec.tsx
+++ src/components/network/LndCard.spec.tsx
@@ -3,16 +3,7 @@ import { renderWithProviders } from 'utils/tests';
 import { LightningNode, Status } from 'types';
 import LndCard from './LndCard';
 
-describe('StatusTag Component', () => {
-  const node: LightningNode = {
-    id: 1,
-    name: 'test lnd',
-    implementation: 'LND',
-    status: Status.Stopped,
-    type: 'lightning',
-    backendName: 'bitcoind-1',
-  };
-
+describe('LND Card Component', () => {
   const details = [
     { label: 'PubKey', value: '0245....5fd47' },
     { label: 'Host', value: '159.65.239.204' },
@@ -21,17 +12,31 @@ describe('StatusTag Component', () => {
     { label: 'Version', value: 'v0.7.1' },
   ];
 
-  const renderComponent = () => {
+  const renderComponent = (status: Status) => {
+    const node: LightningNode = {
+      id: 1,
+      name: 'test lnd',
+      implementation: 'LND',
+      status,
+      type: 'lightning',
+      backendName: 'bitcoind-1',
+    };
+
     return renderWithProviders(<LndCard node={node} details={details} />);
   };
 
   it('should render the name', () => {
-    const { getByText } = renderComponent();
-    expect(getByText(node.name)).toBeTruthy();
+    const { getByText } = renderComponent(Status.Stopped);
+    expect(getByText('test lnd')).toBeTruthy();
   });
 
-  it('should render the details', () => {
-    const { getByText } = renderComponent();
+  it('should display Stopped when node is stopped', () => {
+    const { getByText } = renderComponent(Status.Stopped);
+    expect(getByText('Stopped')).toBeTruthy();
+  });
+
+  it('should render the details when node is started', () => {
+    const { getByText } = renderComponent(Status.Started);
     expect(getByText(details[0].label)).toBeTruthy();
     expect(getByText(details[0].value)).toBeTruthy();
     expect(getByText(details[1].label)).toBeTruthy();
