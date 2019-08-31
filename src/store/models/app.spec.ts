6dce7b4392fc10f1f163f9c9710847b045d24512
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2019-08-31 15:18:19 -0400

    feat(store): create app model with sidebarCollapsed value

---
+++ src/store/models/app.spec.ts
@@ -0,0 +1,25 @@
+import { createStore } from 'easy-peasy';
+import { injections } from 'utils/tests';
+import appModel from './app';
+
+describe('App model', () => {
+  // initialize store for type inference
+  let store = createStore(appModel, { injections });
+
+  beforeEach(() => {
+    // reset the store before each test run
+    store = createStore(appModel, { injections });
+  });
+
+  it('should have a valid initial state', () => {
+    expect(store.getState().sidebarCollapsed).toEqual(false);
+  });
+
+  it('should toggle collapsed value', () => {
+    expect(store.getState().sidebarCollapsed).toEqual(false);
+    store.getActions().collapseSidebar(true);
+    expect(store.getState().sidebarCollapsed).toEqual(true);
+    store.getActions().collapseSidebar(false);
+    expect(store.getState().sidebarCollapsed).toEqual(false);
+  });
+});
--- src/store/models/index.ts
+++ src/store/models/index.ts
@@ -2,16 +2,19 @@ import { connectRouter, RouterState } from 'connected-react-router';
 import { reducer, Reducer } from 'easy-peasy';
 import { History } from 'history';
 import { AnyAction } from 'redux';
+import appModel, { AppModel } from './app';
 import networkModel, { NetworkModel } from './network';
 
 export interface RootModel {
   router: Reducer<RouterState, AnyAction>;
+  app: AppModel;
   network: NetworkModel;
 }
 
 export const createModel = (history: History<any>): RootModel => {
   const rootModel: RootModel = {
     router: reducer(connectRouter(history) as any),
+    app: appModel,
     network: networkModel,
   };
   return rootModel;
