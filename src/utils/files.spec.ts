56deae3c2c173ab9201375c346275f49efeb114d
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2019-08-30 23:35:04 -0400

    feat(files): add readDataFile to files util

--- src/utils/files.spec.ts
+++ src/utils/files.spec.ts
@@ -1,40 +1,69 @@
 import * as fs from 'fs';
 import { join } from 'path';
 import { dataPath } from './config';
-import { writeDataFile } from './files';
+import { readDataFile, writeDataFile } from './files';
 
 jest.mock('fs', () => ({
   promises: {
     mkdir: jest.fn(),
     writeFile: jest.fn(),
+    readFile: jest.fn(() => Buffer.from('test data')),
   },
 }));
 
-const mockFs = fs as jest.Mocked<typeof fs>;
+const mockFs = fs.promises as jest.Mocked<typeof fs.promises>;
 
 describe('Files util', () => {
-  it('should write files to disk in the app data dir', async () => {
-    await writeDataFile('networks/test.txt', 'test data');
-    expect(mockFs.promises.mkdir).toBeCalledTimes(1);
-    expect(mockFs.promises.writeFile).toBeCalledTimes(1);
+  describe('read files', () => {
+    beforeEach(() => {
+      mockFs.readFile.mockResolvedValue(Buffer.from('test data'));
+    });
+
+    it('should read files from disk in the app data dir', async () => {
+      await readDataFile('networks/test.txt');
+      expect(mockFs.readFile).toBeCalledTimes(1);
+    });
+
+    it('should handle relative paths', async () => {
+      const relPath = join('networks', 'test.txt');
+      const absPath = join(dataPath, relPath);
+      await readDataFile(relPath);
+      expect(mockFs.readFile).toBeCalledTimes(1);
+      expect(mockFs.readFile).toBeCalledWith(absPath);
+    });
+
+    it('should handle absolute paths', async () => {
+      const absPath = join(__dirname, 'networks', 'test.txt');
+      await readDataFile(absPath);
+      expect(mockFs.readFile).toBeCalledTimes(1);
+      expect(mockFs.readFile).toBeCalledWith(absPath);
+    });
   });
 
-  it('should handle relative paths', async () => {
-    const relPath = join('networks', 'test.txt');
-    const absPath = join(dataPath, relPath);
-    const data = 'test data';
-    await writeDataFile(relPath, data);
-    expect(mockFs.promises.mkdir).toBeCalledTimes(1);
-    expect(mockFs.promises.writeFile).toBeCalledTimes(1);
-    expect(mockFs.promises.writeFile).toBeCalledWith(absPath, data);
-  });
+  describe('write files', () => {
+    it('should write files to disk in the app data dir', async () => {
+      await writeDataFile('networks/test.txt', 'test data');
+      expect(mockFs.mkdir).toBeCalledTimes(1);
+      expect(mockFs.writeFile).toBeCalledTimes(1);
+    });
 
-  it('should handle absolute paths', async () => {
-    const absPath = join(__dirname, 'networks', 'test.txt');
-    const data = 'test data';
-    await writeDataFile(absPath, data);
-    expect(mockFs.promises.mkdir).toBeCalledTimes(1);
-    expect(mockFs.promises.writeFile).toBeCalledTimes(1);
-    expect(mockFs.promises.writeFile).toBeCalledWith(absPath, data);
+    it('should handle relative paths', async () => {
+      const relPath = join('networks', 'test.txt');
+      const absPath = join(dataPath, relPath);
+      const data = 'test data';
+      await writeDataFile(relPath, data);
+      expect(mockFs.mkdir).toBeCalledTimes(1);
+      expect(mockFs.writeFile).toBeCalledTimes(1);
+      expect(mockFs.writeFile).toBeCalledWith(absPath, data);
+    });
+
+    it('should handle absolute paths', async () => {
+      const absPath = join(__dirname, 'networks', 'test.txt');
+      const data = 'test data';
+      await writeDataFile(absPath, data);
+      expect(mockFs.mkdir).toBeCalledTimes(1);
+      expect(mockFs.writeFile).toBeCalledTimes(1);
+      expect(mockFs.writeFile).toBeCalledWith(absPath, data);
+    });
   });
 });
--- src/utils/files.ts
+++ src/utils/files.ts
@@ -4,7 +4,7 @@ import { dataPath } from './config';
 
 /**
  * Writes data to file located in the app's data directory
- * @param filePath the path to the file, relative to the app's data dir
+ * @param filePath the path to the file. either absolute or relative to the app's data dir
  * @param content the contents of the file
  */
 export const writeDataFile = async (filePath: string, content: string) => {
@@ -12,4 +12,13 @@ export const writeDataFile = async (filePath: string, content: string) => {
   const dirPath = dirname(absPath);
   await fs.mkdir(dirPath, { recursive: true });
   await fs.writeFile(absPath, content);
+};
+
+/**
+ * Reads data from a file located in the app's data directory
+ * @param filePath the path to the file. either absolute or relative to the app's data dir
+ */
+export const readDataFile = async (filePath: string): Promise<string> => {
+  const absPath = isAbsolute(filePath) ? filePath : join(dataPath, filePath);
+  return (await fs.readFile(absPath)).toString();
 };
