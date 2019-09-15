e5a6b2296f16b9ca05c0a5eda26a56f5bf9a5bac
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2019-09-15 02:17:03 -0400

    test(e2e): update e2e tests with new selectors

--- src/components/layouts/AppLayout.spec.tsx
+++ src/components/layouts/AppLayout.spec.tsx
@@ -6,42 +6,43 @@ import AppLayout from './AppLayout';
 
 describe('AppLayout component', () => {
   const renderComponent = (route?: string) => {
+    // create a wrapper component to test language switching
     const LangWrapper: React.FC = () => {
       const { t } = useTranslation();
       return (
         <AppLayout>
           <p>Hello World!</p>
-          <p>{t('cmps.app-layout.new-network')}</p>
+          <p>{t('cmps.home.card-title')}</p>
         </AppLayout>
       );
     };
     return renderWithProviders(<LangWrapper />, { route });
   };
 
-  it('should contain a "Hello World!" text', () => {
+  it('should contain the text of child components', () => {
     const { getByText } = renderComponent();
     expect(getByText('Hello World!')).toBeInTheDocument();
   });
 
   it('should have language set to English by default', () => {
     const { getByText } = renderComponent();
-    expect(getByText('Network')).toBeInTheDocument();
+    expect(getByText('Welcome to Polar')).toBeInTheDocument();
   });
 
   it('should set language to English', () => {
     const { getByText } = renderComponent();
-    expect(getByText('Network')).toBeInTheDocument();
+    expect(getByText('Welcome to Polar')).toBeInTheDocument();
     fireEvent.click(getByText('ES'));
-    expect(getByText('Red')).toBeInTheDocument();
+    expect(getByText('Bienvenido a Polar')).toBeInTheDocument();
     fireEvent.click(getByText('EN'));
-    expect(getByText('Network')).toBeInTheDocument();
+    expect(getByText('Welcome to Polar')).toBeInTheDocument();
   });
 
   it('should set language to Spanish', async () => {
     const { getByText } = renderComponent();
-    expect(getByText('Network')).toBeInTheDocument();
+    expect(getByText('Welcome to Polar')).toBeInTheDocument();
     fireEvent.click(getByText('ES'));
-    expect(getByText('Red')).toBeInTheDocument();
+    expect(getByText('Bienvenido a Polar')).toBeInTheDocument();
   });
 
   it('should navigate to home page when logo clicked', () => {
