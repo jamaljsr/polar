1f67a79c8a2703cfb2f2dfc0910842172f1a1cfa
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2019-09-21 01:04:27 -0400

    test(e2e): update e2e test to use new emotion css selectors

--- e2e/pages/NetworkView.ts
+++ e2e/pages/NetworkView.ts
@@ -2,8 +2,8 @@ import { Selector } from 'testcafe';
 
 class NetworkView {
   backButton = Selector('.ant-page-header-back-button');
-  lndNodes = Selector('[class^=node__]').withText('lnd');
-  bitcoindNodes = Selector('[class^=node__]').withText('bitcoind');
+  lndNodes = Selector('[class*="x-Node "]').withText('lnd');
+  bitcoindNodes = Selector('[class*="x-Node "]').withText('bitcoind');
 
   getLndNodeCount = () => this.lndNodes.count;
   getBitcoindNodeCount = () => this.bitcoindNodes.count;
