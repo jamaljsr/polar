e3d8cbcb943b66207bcc449a2a98893508d8bc4b
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2019-09-21 00:17:25 -0400

    feat(styles): use emotion for global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.less';
+import { css, Global } from '@emotion/core';
 import './i18n';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root: React.FC = () => (
+  <>
+    <Global
+      styles={css`
+        body {
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+            'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+        }
+
+        code {
+          font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+        }
+      `}
+    />
+    <App />
+  </>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
