0f449c084558ede23d2903eb5d60fdca3308fb79
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2019-09-20 22:19:26 -0400

    feat(styles): replace less files with emotion for designer components

--- src/components/designer/CustomNodeInner.tsx
+++ src/components/designer/CustomNodeInner.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import { INodeInnerDefaultProps } from '@mrblenny/react-flow-chart';
 import { StatusBadge } from 'components/common';
-import styles from './CustomNodeInner.module.less';
+
+const Node = styled.div`
+  padding: 20px;
+  text-align: center;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
 
 const CustomNodeInner: React.FC<INodeInnerDefaultProps> = ({ node }) => {
   return (
-    <div className={styles.node}>
+    <Node>
       <span>
         <StatusBadge text={node.id} status={node.properties.status} />
       </span>
       <img src={node.properties.icon} style={{ width: 24, height: 24 }} alt="" />
-    </div>
+    </Node>
   );
 };
 
