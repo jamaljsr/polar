4ba0a3b75902ebe9e040cc8071e4e7b837aa274b
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2019-09-22 00:05:02 -0400

    test(bitcoind): fix broken unit tests

--- src/lib/docker/composeFile.spec.ts
+++ src/lib/docker/composeFile.spec.ts
@@ -16,18 +16,18 @@ describe('ComposeFile', () => {
   });
 
   it('should add multiple services', () => {
-    composeFile.addBitcoind('bitcoind1');
-    composeFile.addLnd('lnd1', 'bitcoind1');
+    composeFile.addBitcoind('bitcoind1', '0.18.1', 18443);
+    composeFile.addLnd('lnd1', '0.7.1', 'bitcoind1', 8080, 10009);
     expect(Object.keys(composeFile.content.services).length).toEqual(2);
   });
 
   it('should add a bitcoind config', () => {
-    composeFile.addBitcoind('bitcoind1');
+    composeFile.addBitcoind('bitcoind1', '0.18.1', 18443);
     expect(composeFile.content.services.bitcoind1).not.toBeUndefined();
   });
 
   it('should create the correct bitcoind docker compose values', () => {
-    composeFile.addBitcoind('bitcoind1');
+    composeFile.addBitcoind('bitcoind1', '0.18.1', 18443);
     const service = composeFile.content.services.bitcoind1;
     expect(service.image).toContain('bitcoind');
     expect(service.container_name).toEqual('bitcoind1');
@@ -35,12 +35,12 @@ describe('ComposeFile', () => {
   });
 
   it('should add an lnd config', () => {
-    composeFile.addLnd('lnd1', 'bitcoind1');
+    composeFile.addLnd('lnd1', '0.7.1', 'bitcoind1', 8080, 10009);
     expect(composeFile.content.services.lnd1).not.toBeUndefined();
   });
 
   it('should create the correct lnd docker compose values', () => {
-    composeFile.addLnd('lnd1', 'bitcoind1');
+    composeFile.addLnd('lnd1', '0.7.1', 'bitcoind1', 8080, 10009);
     const service = composeFile.content.services.lnd1;
     expect(service.image).toContain('lnd');
     expect(service.container_name).toEqual('lnd1');
--- src/lib/docker/nodeTemplates.spec.ts
+++ src/lib/docker/nodeTemplates.spec.ts
@@ -2,14 +2,14 @@ import { bitcoind, lnd } from './nodeTemplates';
 
 describe('nodeTemplates', () => {
   it('should create a valid bitcoind config', () => {
-    const node = bitcoind('mynode');
+    const node = bitcoind('mynode', '0.18.1', 18443);
     expect(node.image).toContain('bitcoind');
     expect(node.container_name).toEqual('mynode');
     expect(node.volumes[0]).toContain('mynode');
   });
 
   it('should create a valid lnd config', () => {
-    const node = lnd('mynode', 'btcnode1');
+    const node = lnd('mynode', '0.7.1-beta', 'btcnode1', 8080, 10009);
     expect(node.image).toContain('lnd');
     expect(node.container_name).toEqual('mynode');
     expect(node.command).toContain('btcnode1');
