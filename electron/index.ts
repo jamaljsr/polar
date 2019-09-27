18245d4a0f9372e7a6984983948cf0d2455da692
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2019-09-27 10:15:05 -0400

    refactor(electron): refactor electron main process entrypoint

--- electron/index.ts
+++ electron/index.ts
@@ -1,76 +1,16 @@
-import { app, BrowserWindow, ipcMain } from 'electron';
 import electronDebug from 'electron-debug';
 import isNotPackaged from 'electron-is-dev';
 import { debug } from 'electron-log';
-import windowState from 'electron-window-state';
-import path from 'path';
-import { initLndProxy } from './lnd/lndProxy';
+import WindowManager from './windowManager';
 
 const isDev = isNotPackaged && process.env.NODE_ENV !== 'production';
+// disable the Electron Security Warnings shown when access the dev url
 process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = `${isDev}`;
+
 debug(`Starting Electron main process`);
 
-let mainWindow: BrowserWindow | null;
+// add keyboard shortcuts and auto open dev tools for all windows
+electronDebug();
 
-// use dev server for hot reload or file in production
-const url = isDev
-  ? 'http://localhost:3000'
-  : `file://${path.join(__dirname, '../build/index.html')}`;
-
-// install react & redux chrome dev tools
-const installExtensions = async () => {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const installer = require('electron-devtools-installer');
-  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-  const extensions = ['REACT_DEVELOPER_TOOLS', 'REDUX_DEVTOOLS'];
-
-  return Promise.all(
-    extensions.map(name => installer.default(installer[name], forceDownload)),
-  ).catch(debug);
-};
-
-const createWindow = async () => {
-  const mainState = windowState({
-    defaultWidth: 900,
-    defaultHeight: 600,
-  });
-
-  mainWindow = new BrowserWindow({
-    x: mainState.x,
-    y: mainState.y,
-    width: mainState.width,
-    height: mainState.height,
-    minWidth: 781,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
-
-  mainState.manage(mainWindow);
-
-  mainWindow.loadURL(url);
-
-  if (isDev) {
-    electronDebug();
-    await installExtensions();
-    mainWindow.webContents.openDevTools();
-  }
-
-  mainWindow.on('closed', () => (mainWindow = null));
-};
-
-app.on('ready', createWindow);
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
-
-initLndProxy(ipcMain);
+const windowManager = new WindowManager(isDev);
+windowManager.start();
---
+++ electron/windowManager.ts
@@ -0,0 +1,81 @@
+import { app, BrowserWindow, ipcMain } from 'electron';
+import windowState from 'electron-window-state';
+import { join } from 'path';
+import { initLndProxy } from './lnd/lndProxy';
+
+const devUrl = 'http://localhost:3000';
+const prodUrl = `file://${join(__dirname, '../build/index.html')}`;
+
+class WindowManager {
+  mainWindow: BrowserWindow | null = null;
+  isDev: boolean;
+
+  constructor(isDev: boolean) {
+    this.isDev = isDev;
+  }
+
+  start() {
+    app.on('ready', () => {
+      this.createWindow();
+      initLndProxy(ipcMain);
+    });
+    app.on('window-all-closed', this.onAllClosed);
+    app.on('activate', this.onActivate);
+  }
+
+  async createWindow() {
+    const mainState = windowState({
+      defaultWidth: 900,
+      defaultHeight: 600,
+    });
+
+    this.mainWindow = new BrowserWindow({
+      x: mainState.x,
+      y: mainState.y,
+      width: mainState.width,
+      height: mainState.height,
+      minWidth: 900,
+      webPreferences: {
+        nodeIntegration: true,
+      },
+    });
+
+    if (this.isDev) {
+      this.setupDevEnv();
+    }
+
+    this.mainWindow.on('closed', this.onMainClosed);
+
+    // use dev server for hot reload or file in production
+    this.mainWindow.loadURL(this.isDev ? devUrl : prodUrl);
+
+    mainState.manage(this.mainWindow);
+  }
+
+  async setupDevEnv() {
+    // install react & redux chrome dev tools
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const installer = require('electron-devtools-installer');
+
+    await installer.default('REACT_DEVELOPER_TOOLS');
+    await installer.default('REDUX_DEVTOOLS');
+  }
+
+  onMainClosed() {
+    this.mainWindow = null;
+  }
+
+  onAllClosed() {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  }
+
+  onActivate() {
+    if (this.mainWindow === null) {
+      this.createWindow();
+    }
+  }
+}
+
+export default WindowManager;
