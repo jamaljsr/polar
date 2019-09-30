1c82c209e4329e15cb35de4600a0aa6a9044d448
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2019-09-30 18:26:55 -0400

    test(utils): add additional unit tests for async utils

---
+++ src/utils/async.spec.ts
@@ -0,0 +1,31 @@
+import { waitFor } from './async';
+
+describe('Async Util', () => {
+  it('should resolve immediately if condition is true', async () => {
+    const condition = jest.fn().mockResolvedValue(true);
+    const result = await waitFor(condition);
+    expect(result).toBe(true);
+    expect(condition).toBeCalledTimes(1);
+  });
+
+  it('should timeout if the condition is never true', async () => {
+    const condition = jest.fn().mockResolvedValue(false);
+    await expect(waitFor(condition, 10, 30)).resolves.toBe(false);
+  });
+
+  it('should resolve once the condition becomes true', async () => {
+    // return false initially
+    const condition = jest.fn().mockResolvedValue(false);
+    // chain the spy onto the promise so we can inspect if its been called
+    const spy = jest.fn(x => x);
+    const promise = waitFor(condition, 10, 100).then(spy);
+    // confirm it isn't called immediately
+    expect(spy).not.toBeCalled();
+    // make condition return true
+    condition.mockResolvedValue(true);
+    // wait for the promise to be resolved
+    await expect(promise).resolves.toBe(true);
+    // confirm the spy was called
+    expect(spy).toBeCalled();
+  });
+});
