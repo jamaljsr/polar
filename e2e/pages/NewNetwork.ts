ca764f592a2722002d524cf03d309e524d6e143d
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2019-09-30 16:16:26 -0400

    test(e2e): fix failing e2e tests

--- e2e/pages/NewNetwork.ts
+++ e2e/pages/NewNetwork.ts
@@ -4,9 +4,6 @@ class NewNetwork {
   nameInput = Selector('input[id=name]');
   lndNodesInput = Selector('input[id=lndNodes]');
   submitBtn = Selector('button[type=submit]').withExactText('Create');
-  notification = Selector('.ant-notification-notice-message');
-
-  getNotificationText = () => this.notification.innerText;
 }
 
 export default new NewNetwork();
