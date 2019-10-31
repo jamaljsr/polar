480f71f94cdc8409d2d0b9606ed1d803a5af5768
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2019-10-31 09:59:38 -0400

    refactor(routing): update variable name for the /network route

--- src/components/common/NavMenu.spec.tsx
+++ src/components/common/NavMenu.spec.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { fireEvent } from '@testing-library/dom';
 import { renderWithProviders } from 'utils/tests';
-import { NETWORK } from 'components/routing';
+import { NETWORK_NEW } from 'components/routing';
 import NavMenu from './NavMenu';
 
 describe('DetailsList Component', () => {
@@ -12,6 +12,6 @@ describe('DetailsList Component', () => {
   it('should navigate to /network when create menu item clicked', () => {
     const { getByText, history } = renderComponent();
     fireEvent.click(getByText('Create Network'));
-    expect(history.location.pathname).toEqual(NETWORK);
+    expect(history.location.pathname).toEqual(NETWORK_NEW);
   });
 });
--- src/components/routing/Routes.tsx
+++ src/components/routing/Routes.tsx
@@ -3,13 +3,13 @@ import { Route } from 'react-router';
 import { Home } from 'components/home';
 import { AppLayout } from 'components/layouts';
 import { NetworkView, NewNetwork } from 'components/network';
-import { HOME, NETWORK, Switch } from 'components/routing';
+import { HOME, NETWORK_NEW, Switch } from 'components/routing';
 
 const Routes: React.FC = () => (
   <AppLayout>
     <Switch>
       <Route path={HOME} exact component={Home} />
-      <Route path={NETWORK} exact component={NewNetwork} />
+      <Route path={NETWORK_NEW} exact component={NewNetwork} />
       <Route path="/network/:id" component={NetworkView} />
     </Switch>
   </AppLayout>
