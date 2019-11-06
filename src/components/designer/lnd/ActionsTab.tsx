95c15e7ae8877ca6d6349ae3f6e65ebf9ea213b4
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2019-11-05 23:46:43 -0500

    fix(network): fix spacing above the remove node button

--- src/components/designer/lnd/ActionsTab.tsx
+++ src/components/designer/lnd/ActionsTab.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import { usePrefixedTranslation } from 'hooks';
+import styled from '@emotion/styled';
 import { LndNode, Status } from 'shared/types';
 import { OpenTerminalButton } from 'components/terminal';
 import { Deposit, OpenChannelButtons, RemoveNode } from './actions';
+
+const Styled = {
+  Spacer: styled.div`
+    height: 48px;
+  `,
+};
 
 interface Props {
   node: LndNode;
 }
 
 const ActionsTab: React.FC<Props> = ({ node }) => {
-  const { l } = usePrefixedTranslation('cmps.designer.lnd.ActionsTab');
-
   return (
     <>
-      {node.status !== Status.Started && l('notStarted')}
       {node.status === Status.Started && (
         <>
           <Deposit node={node} />
           <OpenChannelButtons node={node} />
           <OpenTerminalButton node={node} />
+          <Styled.Spacer />
         </>
       )}
       <RemoveNode node={node} />
--- src/components/designer/lnd/actions/RemoveNode.tsx
+++ src/components/designer/lnd/actions/RemoveNode.tsx
@@ -1,15 +1,8 @@
 import React, { useEffect } from 'react';
-import styled from '@emotion/styled';
 import { Button, Form, Modal } from 'antd';
 import { usePrefixedTranslation } from 'hooks';
 import { LndNode } from 'shared/types';
 import { useStoreActions } from 'store';
-
-const Styled = {
-  FormItem: styled(Form.Item)`
-    margin-top: 48px;
-  `,
-};
 
 interface Props {
   node: LndNode;
@@ -45,11 +38,11 @@ const RemoveNode: React.FC<Props> = ({ node }) => {
   useEffect(() => () => modal && modal.destroy(), [modal]);
 
   return (
-    <Styled.FormItem label={l('title')}>
+    <Form.Item label={l('title')}>
       <Button type="danger" block ghost onClick={showRemoveModal}>
         {l('btnText')}
       </Button>
-    </Styled.FormItem>
+    </Form.Item>
   );
 };
 
