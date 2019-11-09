bc17022b02a3aab10e5dccd58f78a1f7c7be0d01
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2019-11-08 23:04:14 -0500

    fix(docker): update dockerfiles to work on linux

--- docker/bitcoind/docker-entrypoint.sh
+++ docker/bitcoind/docker-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+if ! id bitcoin > /dev/null 2>&1; then
+  echo "adding user bitcoin ($USERID:$GROUPID)"
+  groupadd -g $GROUPID bitcoin
+  useradd -r -u $USERID -g $GROUPID bitcoin 
+fi
+
 if [ $(echo "$1" | cut -c1) = "-" ]; then
   echo "$0: assuming arguments for bitcoind"
 
--- docker/lnd/docker-entrypoint.sh
+++ docker/lnd/docker-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+if ! id lnd > /dev/null 2>&1; then
+  echo "adding user lnd ($USERID:$GROUPID)"
+  groupadd -g $GROUPID lnd
+  useradd -r -u $USERID -g $GROUPID lnd 
+fi
+
 if [ $(echo "$1" | cut -c1) = "-" ]; then
   echo "$0: assuming arguments for lnd"
 
