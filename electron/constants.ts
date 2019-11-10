81b8421065de723410a9a124bf43d6259b0dc345
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2019-11-10 17:12:03 -0500

    fix(electron): fix app icon on linux

--- electron/constants.ts
+++ electron/constants.ts
@@ -2,7 +2,8 @@ import isNotPackaged from 'electron-is-dev';
 import { join } from 'path';
 
 export const IS_DEV = isNotPackaged && process.env.NODE_ENV !== 'production';
+export const APP_ROOT = join(__dirname, '..', '..', '..');
 
 export const BASE_URL_DEV = 'http://localhost:3000';
-export const BASE_URL_PROD = `file://${join(__dirname, '../../../build/index.html')}`;
+export const BASE_URL_PROD = `file://${join(APP_ROOT, 'build', 'index.html')}`;
 export const BASE_URL = IS_DEV ? BASE_URL_DEV : BASE_URL_PROD;
