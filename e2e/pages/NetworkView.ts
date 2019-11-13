63ea77b514b78c8efb977b6f650a31cdc35f5fca
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2019-11-13 10:10:24 -0500

    test(e2e): fix broken e2e tests

--- e2e/pages/NetworkView.ts
+++ e2e/pages/NetworkView.ts
@@ -3,12 +3,12 @@ import { Selector } from 'testcafe';
 class NetworkView {
   backButton = Selector('.ant-page-header-back-button');
   headingTitle = Selector('.ant-page-header-heading-title');
-  lndNodes = Selector('[class*="x-Node "]').withText('lnd');
-  bitcoindNodes = Selector('[class*="x-Node "]').withText('bitcoind');
+  aliceNode = Selector('[class*="x-Node "]').withText('alice');
+  bobNode = Selector('[class*="x-Node "]').withText('bob');
+  carolNode = Selector('[class*="x-Node "]').withText('carol');
+  backendNode = Selector('[class*="x-Node "]').withText('backend');
 
   getHeadingTitleText = () => this.headingTitle.innerText;
-  getLndNodeCount = () => this.lndNodes.count;
-  getBitcoindNodeCount = () => this.bitcoindNodes.count;
 }
 
 export default new NetworkView();
