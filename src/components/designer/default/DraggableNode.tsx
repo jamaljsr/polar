b6897ee1eadc0269e02adb6d88f44bfcef8f7d76
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2019-12-13 10:57:24 -0500

    feat(sidebar): animat draggable nodes when shown

--- src/components/designer/default/DraggableNode.tsx
+++ src/components/designer/default/DraggableNode.tsx
@@ -6,13 +6,23 @@ const Styled = {
   Node: styled.div`
     display: flex;
     justify-content: space-between;
-    alignt-items: center;
-    margin: 20px 0;
-    padding: 10px 10px;
     border: 1px solid #e8e8e8;
     border-radius: 4px;
     box-shadow: 4px 2px 9px rgba(0, 0, 0, 0.1);
     cursor: move;
+    overflow: hidden;
+    transition: all 0.3s ease-out;
+    opacity: 1;
+    height: 46px;
+    padding: 10px 10px;
+    margin-top: 20px;
+
+    &.hide {
+      opacity: 0;
+      height: 0;
+      padding: 0;
+      margin-top: 0;
+    }
   `,
   Label: styled.span`
     flex: 1;
@@ -34,15 +44,17 @@ interface Props {
   desc?: string;
   icon: string;
   properties: any;
+  visible: boolean;
 }
 
-const DraggableNode: React.FC<Props> = ({ label, desc, icon, properties }) => {
+const DraggableNode: React.FC<Props> = ({ label, desc, icon, properties, visible }) => {
   return (
     <Styled.Node
       draggable
       onDragStart={event => {
         event.dataTransfer.setData(REACT_FLOW_CHART, JSON.stringify(properties));
       }}
+      className={!visible ? 'hide' : ''}
     >
       <Styled.Label>
         {label} {desc && <Styled.Desc>{desc}</Styled.Desc>}
