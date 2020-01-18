1cd21cddc6a9f394afcf3c0295b1f50cbebf68a2
Umar Bolatov <bolatovumar@gmail.com>
2020-01-17 20:48:18 -0800

    fix(ui): make SidebarCard scrollable
    fix #277

--- src/components/designer/SidebarCard.tsx
+++ src/components/designer/SidebarCard.tsx
@@ -9,5 +9,25 @@ export default styled(Card)`
   width: 360px;
   border-radius: 2px;
   box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1);
-  overflow: hidden;
+  overflow: auto;
+
+  &::-webkit-scrollbar {
+    width: 8px;
+    background-color: rgba(0, 0, 0, 0);
+    border-radius: 10px;
+  }
+
+  &::-webkit-scrollbar:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+
+  &::-webkit-scrollbar-thumb:vertical {
+    background-color: rgba(0, 0, 0, 0.2);
+    border-radius: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb:vertical:active {
+    background-color: rgba(0, 0, 0, 0.6);
+    border-radius: 10px;
+  }
 `;
