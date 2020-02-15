169f57bda5e49e9ce464bb9abae03d786cdb669a
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2020-02-15 00:43:50 -0500

    feat(images): save managed node custom commands to docker compose file

--- src/lib/docker/composeFile.ts
+++ src/lib/docker/composeFile.ts
@@ -1,4 +1,5 @@
 import { BitcoinNode, CLightningNode, CommonNode, LndNode } from 'shared/types';
+import { bitcoinCredentials, dockerConfigs } from 'utils/constants';
 import { getContainerName } from 'utils/network';
 /* eslint-disable @typescript-eslint/camelcase */
 import { bitcoind, clightning, lnd } from './nodeTemplates';
@@ -34,52 +35,82 @@ class ComposeFile {
 
   addBitcoind(node: BitcoinNode) {
     const { name, version, ports } = node;
+    const { rpc, zmqBlock, zmqTx } = ports;
     const container = getContainerName(node);
+    // define the variable substitutions
+    const variables = {
+      rpcUser: bitcoinCredentials.user,
+      rpcAuth: bitcoinCredentials.rpcauth,
+    };
+    // use the node's custom command of the default for the implementation
+    const nodeCommand = node.docker.command || dockerConfigs.bitcoind.command;
+    // replace the variables in the command
+    const command = this.mergeCommand(nodeCommand, variables);
+    // add the docker service
     this.content.services[name] = bitcoind(
       name,
       container,
       version,
-      ports.rpc,
-      ports.zmqBlock,
-      ports.zmqTx,
+      rpc,
+      zmqBlock,
+      zmqTx,
+      command,
     );
   }
 
   addLnd(node: LndNode, backend: CommonNode) {
-    const {
-      name,
-      version,
-      ports: { rest, grpc, p2p },
-    } = node;
+    const { name, version, ports } = node;
+    const { rest, grpc, p2p } = ports;
     const container = getContainerName(node);
-    const backendName = getContainerName(backend);
-    this.content.services[name] = lnd(
-      name,
-      container,
-      version,
-      backendName,
-      rest,
-      grpc,
-      p2p,
-    );
+    // define the variable substitutions
+    const variables = {
+      name: node.name,
+      backendName: getContainerName(backend),
+      rpcUser: bitcoinCredentials.user,
+      rpcPass: bitcoinCredentials.pass,
+    };
+    // use the node's custom command of the default for the implementation
+    const nodeCommand = node.docker.command || dockerConfigs.LND.command;
+    // replace the variables in the command
+    const command = this.mergeCommand(nodeCommand, variables);
+    // add the docker service
+    this.content.services[name] = lnd(name, container, version, rest, grpc, p2p, command);
   }
 
   addClightning(node: CLightningNode, backend: CommonNode) {
-    const {
-      name,
-      version,
-      ports: { rest, p2p },
-    } = node;
+    const { name, version, ports } = node;
+    const { rest, p2p } = ports;
     const container = getContainerName(node);
-    const backendName = getContainerName(backend);
+    // define the variable substitutions
+    const variables = {
+      name: node.name,
+      backendName: getContainerName(backend),
+      rpcUser: bitcoinCredentials.user,
+      rpcPass: bitcoinCredentials.pass,
+    };
+    // use the node's custom command of the default for the implementation
+    const nodeCommand = node.docker.command || dockerConfigs['c-lightning'].command;
+    // replace the variables in the command
+    const command = this.mergeCommand(nodeCommand, variables);
+    // add the docker service
     this.content.services[name] = clightning(
       name,
       container,
       version,
-      backendName,
       rest,
       p2p,
+      command,
     );
+  }
+
+  private mergeCommand(command: string, variables: Record<string, string>) {
+    let merged = command;
+    Object.keys(variables).forEach(key => {
+      // intentionally not using .replace() because if a string is passed in, then only the first occurrence
+      // is replaced. A RegExp could be used but the code would be more confusing because of escape chars
+      merged = merged.split(`{{${key}}}`).join(variables[key]);
+    });
+    return merged;
   }
 }
 
