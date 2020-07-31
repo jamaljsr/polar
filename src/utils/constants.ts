59bce40400800cc4b8f86fc49b17ea9e7b0b6358
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2020-07-31 10:24:15 -0400

    feat(images): bump repo state version

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -213,7 +213,7 @@ export const REPO_STATE_URL =
  * are pushed to Docker Hub, this list should be updated along with the /docker/nodes.json file.
  */
 export const defaultRepoState: DockerRepoState = {
-  version: 11,
+  version: 12,
   images: {
     LND: {
       latest: '0.10.3-beta',
