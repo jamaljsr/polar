57fc766c92289e01f1dce199a278e89ea121a313
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2021-01-26 23:01:32 -0500

    feat(images): add support for LND v0.12.0-beta

--- README.md
+++ README.md
@@ -36,7 +36,7 @@ With Polar you can:
 
 Supported Network Node Versions:
 
-- LND v0.11.1, v0.11.0, v0.10.3, v0.10.2, v0.10.1, v0.10.0, v0.9.1, v0.9.0, v0.8.2, v0.8.0 & v0.7.1
+- LND v0.12.0, v0.11.1, v0.11.0, v0.10.3, v0.10.2, v0.10.1, v0.10.0, v0.9.1, v0.9.0, v0.8.2, v0.8.0 & v0.7.1
 - c-lightning v0.9.2, v0.9.1, v0.9.0, v0.8.2, v0.8.1 & v0.8.0
 - Eclair v0.4.2, v0.3.3
 - Bitcoin Core v0.20.1, v0.20.0, v0.19.1, v0.19.0.1 & v0.18.1
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -216,12 +216,12 @@ export const REPO_STATE_URL =
  * are pushed to Docker Hub, this list should be updated along with the /docker/nodes.json file.
  */
 export const defaultRepoState: DockerRepoState = {
-  version: 18,
+  version: 19,
   images: {
     LND: {
-      latest: '0.11.1-beta',
+      latest: '0.12.0-beta',
       versions: [
-        '0.12.0-beta.rc1',
+        '0.12.0-beta',
         '0.11.1-beta',
         '0.11.0-beta',
         '0.10.3-beta',
@@ -237,7 +237,7 @@ export const defaultRepoState: DockerRepoState = {
       // not all LND versions are compatible with all bitcoind versions.
       // this mapping specifies the highest compatible bitcoind for each LND version
       compatibility: {
-        '0.12.0-beta.rc1': '0.20.1',
+        '0.12.0-beta': '0.20.1',
         '0.11.1-beta': '0.20.1',
         '0.11.0-beta': '0.20.1',
         '0.10.3-beta': '0.20.1',
