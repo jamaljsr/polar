80ba9f55e993da4aa5389d5340eb1c6e0dfb6223
Andrew Oseen <amovfx@protonmail.com>
2023-02-08 21:59:25 -0800

    fix(refactor): test
    Cleaned up the tests

--- src/components/designer/taro/actions/SendAssetModal.spec.tsx
+++ src/components/designer/taro/actions/SendAssetModal.spec.tsx
@@ -27,7 +27,9 @@ describe('SendAssetModal', () => {
       taro: {
         nodes: {
           'alice-taro': {
-            assets: [defaultTaroAsset({ id: 'test1234', name: 'testAsset' })],
+            assets: [
+              defaultTaroAsset({ id: 'test1234', name: 'testAsset', amount: '1000' }),
+            ],
             balances: [],
           },
         },
@@ -60,50 +62,128 @@ describe('SendAssetModal', () => {
 
   afterEach(() => unmount());
 
-  it('should render labels', async () => {
-    const { getByLabelText } = await renderComponent();
-    expect(getByLabelText('Send Asset from alice-taro')).toBeInTheDocument();
-  });
+  describe('When the modal displays', () => {
+    it('should render labels', async () => {
+      const { getByLabelText } = await renderComponent();
+      expect(getByLabelText('Send Asset from alice-taro')).toBeInTheDocument();
+    });
 
-  it('should hide modal when cancel is clicked', async () => {
-    const { getByText, queryByText, store } = await renderComponent();
-    const btn = getByText('Cancel');
-    expect(btn).toBeInTheDocument();
-    expect(btn.parentElement).toBeInstanceOf(HTMLButtonElement);
-    fireEvent.click(btn);
-    await waitFor(() => {
-      expect(store.getState().modals.sendAsset.visible).toBe(false);
-      expect(queryByText('Cancel')).not.toBeInTheDocument();
+    it('should hide when cancel is clicked', async () => {
+      const { getByText, queryByText, store } = await renderComponent();
+      const btn = getByText('Cancel');
+      expect(btn).toBeInTheDocument();
+      expect(btn.parentElement).toBeInstanceOf(HTMLButtonElement);
+      fireEvent.click(btn);
+      await waitFor(() => {
+        expect(store.getState().modals.sendAsset.visible).toBe(false);
+        expect(queryByText('Cancel')).not.toBeInTheDocument();
+      });
     });
   });
-
-  describe('taro lnd node low balance should display alert', () => {
-    it('alert should be present', async () => {
+  describe('When lnd balance is low', () => {
+    beforeEach(() => {
       lightningServiceMock.getBalances.mockResolvedValue({
         confirmed: '100',
         unconfirmed: '200',
         total: '300',
       });
-      const { findByText, getByLabelText, getByText } = await renderComponent();
       taroServiceMock.decodeAddress.mockResolvedValue(
         defaultTaroAddress({ id: 'test1234', type: 'NORMAL', amount: '100' }),
       );
-      const input = getByLabelText('Taro Address');
-      expect(input).toBeInTheDocument();
-      fireEvent.change(input, { target: { value: 'asdfghdfsddasdf' } });
+    });
+    it('an alert should display', async () => {
+      const { findByText, getByLabelText, getByText } = await renderComponent();
       expect(
         await findByText('Insufficient balance on lnd node alice'),
       ).toBeInTheDocument();
-      expect(getByLabelText('Deposit enough funds to alice')).toBeInTheDocument();
+    });
+    it('auto deposit should be present', async () => {
+      const { findByText, getByText } = await renderComponent();
+      expect(
+        await findByText('Insufficient balance on lnd node alice'),
+      ).toBeInTheDocument();
+      expect(getByText('Deposit enough funds to alice')).toBeInTheDocument();
+    });
+    it('a clicked auto deposit should disable the alert', async () => {
+      const { queryByText, getByLabelText, getByText } = await renderComponent();
+      await waitFor(() => {
+        expect(lightningServiceMock.getBalances).toBeCalled();
+      });
       fireEvent.click(getByText('Deposit enough funds to alice'));
+      expect(
+        queryByText('Insufficient balance on lnd node alice'),
+      ).not.toBeInTheDocument();
+    });
+    it('should call new address when send is clicked', async () => {
+      taroServiceMock.decodeAddress.mockResolvedValue(
+        defaultTaroAddress({ id: 'test1234', type: 'NORMAL', amount: '100' }),
+      );
+      const { getByLabelText, getByText } = await renderComponent();
+      await waitFor(() => {
+        expect(lightningServiceMock.getBalances).toBeCalled();
+      });
+      const input = getByLabelText('Taro Address');
+      expect(input).toBeInTheDocument();
+      fireEvent.change(input, { target: { value: 'taro1address' } });
+      await waitFor(() => {
+        expect(taroServiceMock.decodeAddress).toBeCalled();
+      });
+      fireEvent.click(getByText('Deposit enough funds to alice'));
+
+      expect(getByText('Address Info')).toBeInTheDocument();
       fireEvent.click(getByText('Send'));
       await waitFor(() => {
         expect(lightningServiceMock.getNewAddress).toHaveBeenCalled();
       });
     });
   });
+  describe('When the lnd balance is high', () => {
+    beforeEach(() => {
+      lightningServiceMock.getBalances.mockResolvedValue({
+        confirmed: '100000',
+        unconfirmed: '200000',
+        total: '300000',
+      });
+      taroServiceMock.decodeAddress.mockResolvedValue(
+        defaultTaroAddress({ id: 'test1234', type: 'NORMAL', amount: '100' }),
+      );
+    });
+    it('should not display an alert or auto deposit', async () => {
+      const { queryByText, getByLabelText } = await renderComponent();
+      expect(
+        queryByText('Insufficient balance on lnd node alice'),
+      ).not.toBeInTheDocument();
+      expect(queryByText('Deposit enough funds to alice')).not.toBeInTheDocument();
+    });
+    it('should send the asset when send is clicked', async () => {
+      taroServiceMock.decodeAddress.mockResolvedValue(
+        defaultTaroAddress({ id: 'test1234', type: 'NORMAL', amount: '100' }),
+      );
+      const { getByLabelText, getByText } = await renderComponent();
+      const input = getByLabelText('Taro Address');
+      expect(input).toBeInTheDocument();
+      fireEvent.change(input, { target: { value: 'taro1address' } });
+      fireEvent.click(getByText('Send'));
+      await waitFor(() => {
+        expect(taroServiceMock.decodeAddress).toHaveBeenCalled();
+      });
+      expect(getByText('Address Info')).toBeInTheDocument();
+      expect(getByText('Name')).toBeInTheDocument();
+      expect(getByText('testAsset')).toBeInTheDocument();
+      expect(getByText('Type')).toBeInTheDocument();
+      expect(getByText('NORMAL')).toBeInTheDocument();
+      expect(getByText('Amount')).toBeInTheDocument();
+      expect(getByText('100')).toBeInTheDocument();
+      fireEvent.click(getByText('Send'));
 
-  describe('successful decode', () => {
+      await waitFor(() => {
+        expect(taroServiceMock.sendAsset).toHaveBeenCalled();
+        expect(bitcoindServiceMock.mine).toHaveBeenCalled();
+      });
+    });
+  });
+
+  describe('When an address is successfully decoded', () => {
     it('should display assets information and have a successful send', async () => {
       taroServiceMock.decodeAddress.mockResolvedValue(
         defaultTaroAddress({ id: 'test1234', type: 'NORMAL', amount: '100' }),
@@ -130,14 +210,14 @@ describe('SendAssetModal', () => {
       });
     });
 
-    it('with missing asset should warn', async () => {
+    it('and the asset is missing it should warn', async () => {
       taroServiceMock.decodeAddress.mockResolvedValue(
         defaultTaroAddress({ id: 'badid' }),
       );
       const { getByText, getByLabelText } = await renderComponent();
       const input = getByLabelText('Taro Address');
       expect(input).toBeInTheDocument();
-      fireEvent.change(input, { target: { value: 'asdfghdfsddasdf' } });
+      fireEvent.change(input, { target: { value: 'taro1address' } });
       await waitFor(() => {
         expect(taroServiceMock.decodeAddress).toHaveBeenCalled();
       });
@@ -147,9 +227,11 @@ describe('SendAssetModal', () => {
     });
   });
 
-  describe('unsuccessful decode', () => {
+  describe('An unsuccessful decode', () => {
+    beforeEach(() => {
+      taroServiceMock.decodeAddress.mockRejectedValue(new Error('Error message'));
+    });
     it('should display error', async () => {
-      taroServiceMock.decodeAddress.mockRejectedValueOnce(new Error('bad'));
       const { getByText, getByLabelText } = await renderComponent();
       const input = getByLabelText('Taro Address');
       expect(input).toBeInTheDocument();
@@ -159,10 +241,23 @@ describe('SendAssetModal', () => {
       });
       expect(getByText('ERROR: Invalid Address')).toBeInTheDocument();
     });
+    it('error clears when taro address field is reset', async () => {
+      const { getByText, getByLabelText, queryByText } = await renderComponent();
+      const input = getByLabelText('Taro Address');
+      expect(input).toBeInTheDocument();
+      fireEvent.change(input, { target: { value: 'taro1...' } });
+
+      await waitFor(() => {
+        expect(taroServiceMock.decodeAddress).toHaveBeenCalled();
+      });
+      expect(getByText('ERROR: Invalid Address')).toBeInTheDocument();
+      fireEvent.change(input, { target: { value: '' } });
+      expect(queryByText('ERROR: Invalid Address')).toBeNull();
+    });
   });
 
-  describe('send error', () => {
-    it('should display error', async () => {
+  describe('When there is a send error', () => {
+    it('should display an error', async () => {
       taroServiceMock.decodeAddress.mockResolvedValue(
         defaultTaroAddress({ id: 'test1234', type: 'NORMAL', amount: '100' }),
       );
@@ -179,28 +274,6 @@ describe('SendAssetModal', () => {
         expect(taroServiceMock.sendAsset).toHaveBeenCalled();
       });
       expect(getByText('Error message')).toBeInTheDocument();
-    });
-  });
-
-  describe('clear error', () => {
-    it('should clear help error', async () => {
-      taroServiceMock.decodeAddress.mockRejectedValue(new Error('Error message'));
-      lightningServiceMock.getBalances.mockResolvedValue({
-        confirmed: '100',
-        unconfirmed: '200',
-        total: '300',
-      });
-      const { getByText, getByLabelText, queryByText } = await renderComponent();
-      const input = getByLabelText('Taro Address');
-      expect(input).toBeInTheDocument();
-      fireEvent.change(input, { target: { value: 'taro1aaa...' } });
-
-      await waitFor(() => {
-        expect(taroServiceMock.decodeAddress).toHaveBeenCalled();
-      });
-      expect(getByText('ERROR: Invalid Address')).toBeInTheDocument();
-      fireEvent.change(input, { target: { value: '' } });
-      expect(queryByText('ERROR: Invalid Address')).toBeNull();
     });
   });
 });
