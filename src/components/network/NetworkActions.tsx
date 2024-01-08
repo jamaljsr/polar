751f7ace38587528f3a604dce246ee68cecd55ca
Kelvin Isievwore <kelvinator4leo@gmail.com>
2024-01-08 11:31:14 +0100

    refactor: used string type instead of any

--- src/components/network/NetworkActions.tsx
+++ src/components/network/NetworkActions.tsx
@@ -96,7 +96,7 @@ const NetworkActions: React.FC<Props> = ({
 
   const mineAsync = useMiningAsync(network);
 
-  const handleClick: MenuProps['onClick'] = useCallback((info: { key: any }) => {
+  const handleClick: MenuProps['onClick'] = useCallback((info: { key: string }) => {
     switch (info.key) {
       case 'rename':
         onRenameClick();
