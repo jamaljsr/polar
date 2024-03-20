655413a0609cc772c2925be2a37b6e5626a438a3
Kelvin Isievwore <kelvinator4leo@gmail.com>
2024-03-20 21:57:28 +0100

    test: updated util tests

--- src/utils/tests/renderWithProviders.tsx
+++ src/utils/tests/renderWithProviders.tsx
@@ -20,7 +20,9 @@ export const lightningServiceMock: jest.Mocked<LightningService> = {
   createInvoice: jest.fn(),
   payInvoice: jest.fn(),
   waitUntilOnline: jest.fn(),
-  getChannelListener: jest.fn(),
+  addListenerToNode: jest.fn(),
+  removeListener: jest.fn(),
+  subscribeChannelEvents: jest.fn(),
 };
 export const tapServiceMock: jest.Mocked<TapService> = {
   listAssets: jest.fn(),
