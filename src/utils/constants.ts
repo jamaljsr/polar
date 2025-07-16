1f17cbe0e83aff0059c2ab98c8794df8daa71ede
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2025-07-16 15:04:37 -0500

    feat(images): add support for LND v0.19.2-beta

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -341,11 +341,12 @@ export const REPO_STATE_URL =
  * are pushed to Docker Hub, this list should be updated along with the /docker/nodes.json file.
  */
 export const defaultRepoState: DockerRepoState = {
-  version: 74,
+  version: 75,
   images: {
     LND: {
-      latest: '0.19.1-beta',
+      latest: '0.19.2-beta',
       versions: [
+        '0.19.2-beta',
         '0.19.1-beta',
         '0.19.0-beta',
         '0.18.5-beta',
@@ -360,6 +361,7 @@ export const defaultRepoState: DockerRepoState = {
       // not all LND versions are compatible with all bitcoind versions.
       // this mapping specifies the highest compatible bitcoind for each LND version
       compatibility: {
+        '0.19.2-beta': '29.0',
         '0.19.1-beta': '29.0',
         '0.19.0-beta': '29.0',
         '0.18.5-beta': '29.0',
