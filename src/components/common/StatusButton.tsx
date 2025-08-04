0d75e97ad751ab2dcf113829fcd593bfa6e4666e
Abdullahi Yunus <abdoollahikbk@gmail.com>
2025-08-05 00:16:45 +0100

    refactor: extract status button
    In this commit we extract status button logic into a component
    so that we can reuse it in other places.

---
+++ src/components/common/StatusButton.tsx
@@ -0,0 +1,75 @@
+import React, { ReactNode } from 'react';
+import styled from '@emotion/styled';
+import { Button } from 'antd';
+import { Status } from 'shared/types';
+import { ButtonType } from 'antd/lib/button';
+import { PlayCircleOutlined, StopOutlined, WarningOutlined } from '@ant-design/icons';
+
+const Styled = {
+  Button: styled(Button)`
+    margin-left: 0;
+  `,
+};
+
+interface StatusButtonProps {
+  status: Status;
+  onClick: () => void;
+}
+
+const config: {
+  [key: number]: {
+    label: string;
+    type: ButtonType;
+    danger?: boolean;
+    icon: ReactNode;
+  };
+} = {
+  [Status.Starting]: {
+    label: 'Starting',
+    type: 'primary',
+    icon: '',
+  },
+  [Status.Started]: {
+    label: 'Stop',
+    type: 'primary',
+    danger: true,
+    icon: <StopOutlined />,
+  },
+  [Status.Stopping]: {
+    label: 'Stopping',
+    type: 'default',
+    icon: '',
+  },
+  [Status.Stopped]: {
+    label: 'Start',
+    type: 'primary',
+    icon: <PlayCircleOutlined />,
+  },
+  [Status.Error]: {
+    label: 'Restart',
+    type: 'primary',
+    danger: true,
+    icon: <WarningOutlined />,
+  },
+};
+
+const StatusButton: React.FC<StatusButtonProps> = ({ status, onClick }) => {
+  const { type, danger, icon, label } = config[status];
+  const ghost = status === Status.Started;
+  const loading = status === Status.Starting || status === Status.Stopping;
+  return (
+    <Styled.Button
+      key={label}
+      type={type}
+      danger={danger}
+      icon={icon}
+      loading={loading}
+      ghost={ghost}
+      onClick={onClick}
+    >
+      {label}
+    </Styled.Button>
+  );
+};
+
+export default StatusButton;
