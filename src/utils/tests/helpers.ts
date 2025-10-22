83bb7fcade25216e007cc040f528c95e122f8f65
Kyle 🐆 <kyle@privkey.io>
2025-10-22 19:30:20 -0400

    test: update test files for bitcoind-knots support

--- src/utils/tests/helpers.ts
+++ src/utils/tests/helpers.ts
@@ -190,6 +190,10 @@ export const testRepoState: DockerRepoState = {
         '0.18.1',
       ],
     },
+    'bitcoind-knots': {
+      latest: '29.0',
+      versions: ['29.0', '28.0', '27.0', '26.0'],
+    },
     btcd: {
       latest: '',
       versions: [],
@@ -244,6 +248,7 @@ export const getNetwork = (
     clightningNodes: 1,
     eclairNodes: 1,
     bitcoindNodes: 1,
+    bitcoindKnotsNodes: 0,
     tapdNodes: 0,
     litdNodes: 0,
     status,
